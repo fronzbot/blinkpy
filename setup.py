24158638efe317fffece9f7facaa4cc62f213da5
Kevin Fronczak <kfronczak@gmail.com>
2018-05-09 10:00:13 -0400

    Fix hound

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from os.path import abspath, dirname, join
 from setuptools import setup, find_packages
-# Fixes import error due to breaking change in pip 10
-# https://stackoverflow.com/a/49867265
-try: # pip > 10
+#  Fixes import error due to breaking change in pip 10
+#  https://stackoverflow.com/a/49867265
+try:  #  pip > 10
     from pip._internal.req import parse_requirements
-except ImportError: # pip < 10
+except ImportError:  #  pip < 10
     from pip.req import parse_requirements
 from blinkpy.helpers.constants import (
     __version__, PROJECT_PACKAGE_NAME, PROJECT_LICENSE, PROJECT_URL,
