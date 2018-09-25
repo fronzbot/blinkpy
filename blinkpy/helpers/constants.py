411da60bb2231bd85fc21fc47dac3e246acf0f50
Kevin Fronczak <kfronczak@gmail.com>
2018-09-24 22:30:17 -0400

    version_bump

--- blinkpy/helpers/constants.py
+++ blinkpy/helpers/constants.py
@@ -3,8 +3,8 @@
 import os
 
 MAJOR_VERSION = 0
-MINOR_VERSION = 9
-PATCH_VERSION = '0.dev'
+MINOR_VERSION = 8
+PATCH_VERSION = 3
 
 __version__ = '{}.{}.{}'.format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 
