7710e3f972d0b96c37d9f289bdda992c9551f853
Kevin Fronczak <kfronczak@gmail.com>
2018-10-30 21:08:43 -0400

    Errant whitespace

--- blinkpy/helpers/constants.py
+++ blinkpy/helpers/constants.py
@@ -4,7 +4,7 @@ import os
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 11
-PATCH_VERSION = '0.dev' 
+PATCH_VERSION = '0.dev'
 
 __version__ = '{}.{}.{}'.format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 
