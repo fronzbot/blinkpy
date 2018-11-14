9c1974c227c1b8b6d7cd6a086c2ff23657bd8d82
md-reddevil <gstarshii@gmail.com>
2018-11-14 09:39:44 +0200

    Add method to request the Network/System status
    Home screen method returns just the information about last added System(network). The method "request_network_status", should replace the homescreen method in order to obtain all the information about a system.

--- blinkpy/api.py
+++ blinkpy/api.py
@@ -29,6 +29,11 @@ def request_networks(blink):
     url = "{}/networks".format(blink.urls.base_url)
     return http_get(blink, url)
 
+def request_network_status(blink, network):
+    """Request network information."""
+    url = "{}/network/{}".format(blink.urls.base_url, network)
+    return http_get(blink, url)
+
 
 def request_syncmodule(blink, network):
     """Request sync module info."""
