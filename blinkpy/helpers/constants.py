fd64f41b22f0f32c1fb27702848b41c34e4440b8
Kevin Fronczak <kfronczak@gmail.com>
2019-01-23 10:55:39 -0500

    Update constants.py

--- blinkpy/helpers/constants.py
+++ blinkpy/helpers/constants.py
@@ -3,8 +3,8 @@
 import os
 
 MAJOR_VERSION = 0
-MINOR_VERSION = 12
-PATCH_VERSION = '0.dev'
+MINOR_VERSION = 11
+PATCH_VERSION = 2
 
 __version__ = '{}.{}.{}'.format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 
