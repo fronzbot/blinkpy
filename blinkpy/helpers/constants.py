b201c95e8d893da12a13f712d9787505cd5574bb
Kevin Fronczak <kfronczak@gmail.com>
2020-05-04 13:37:32 -0400

    Remove Python 3.5.3 support

--- blinkpy/helpers/constants.py
+++ blinkpy/helpers/constants.py
@@ -30,7 +30,6 @@ PROJECT_CLASSIFIERS = [
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
