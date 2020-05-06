dfb90b8c86fb8a67171808739b6a94082905548c
Kevin Fronczak <kfronczak@gmail.com>
2020-05-06 10:43:09 -0400

    Dev version bump

--- blinkpy/helpers/constants.py
+++ blinkpy/helpers/constants.py
@@ -4,7 +4,7 @@ import os
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 15
-PATCH_VERSION = "0-rc.0"
+PATCH_VERSION = "0-rc.1"
 
 __version__ = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 
