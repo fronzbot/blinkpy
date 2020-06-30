24ca9d961741ff9a91b5584a08e71a7abce66bde
Kevin Fronczak <kfronczak@gmail.com>
2020-06-30 16:40:19 +0000

    Fixed request version, improved setup.py requirement import

--- setup.py
+++ setup.py
@@ -3,23 +3,26 @@
 from os.path import abspath, dirname
 from setuptools import setup, find_packages
 from blinkpy.helpers.constants import (
-    __version__, PROJECT_PACKAGE_NAME, PROJECT_LICENSE, PROJECT_URL,
-    PROJECT_EMAIL, PROJECT_DESCRIPTION, PROJECT_CLASSIFIERS, PROJECT_AUTHOR,
+    __version__,
+    PROJECT_PACKAGE_NAME,
+    PROJECT_LICENSE,
+    PROJECT_URL,
+    PROJECT_EMAIL,
+    PROJECT_DESCRIPTION,
+    PROJECT_CLASSIFIERS,
+    PROJECT_AUTHOR,
 )
 
 PROJECT_VERSION = __version__
 
 THIS_DIR = abspath(dirname(__file__))
 
-REQUIRES = [
-    "python-dateutil~=2.8.1",
-    "requests~=2.23.0",
-    "python-slugify~=4.0.0",
-]
+with open(f"{THIS_DIR}/requirements.txt") as req_file:
+    REQUIRES = [line.rstrip() for line in req_file]
 
-PACKAGES = find_packages(exclude=['tests*', 'docs'])
+PACKAGES = find_packages(exclude=["tests*", "docs"])
 
-with open('{}/README.rst'.format(THIS_DIR), encoding='utf-8') as readme_file:
+with open("{}/README.rst".format(THIS_DIR), encoding="utf-8") as readme_file:
     LONG_DESCRIPTION = readme_file.read()
 
 setup(
@@ -31,11 +34,11 @@ setup(
     author_email=PROJECT_EMAIL,
     license=PROJECT_LICENSE,
     url=PROJECT_URL,
-    platforms='any',
-    py_modules=['blinkpy'],
+    platforms="any",
+    py_modules=["blinkpy"],
     packages=PACKAGES,
     include_package_data=True,
     install_requires=REQUIRES,
-    test_suite='tests',
-    classifiers=PROJECT_CLASSIFIERS
+    test_suite="tests",
+    classifiers=PROJECT_CLASSIFIERS,
 )
