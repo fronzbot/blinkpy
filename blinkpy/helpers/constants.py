4990b384f654c6ca9efc0e1bff9b6cd2da08e6e5
Kevin Fronczak <kfronczak@gmail.com>
2022-03-12 13:14:31 -0500

    Update constants.py

--- blinkpy/helpers/constants.py
+++ blinkpy/helpers/constants.py
@@ -35,7 +35,6 @@ PROJECT_CLASSIFIERS = [
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
-    "Programming Language :: Python :: 3.11",
     "Topic :: Home Automation",
 ]
 
