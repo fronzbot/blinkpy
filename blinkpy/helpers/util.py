ca5c2e8cd96c213ad567ad74ab914e5be1e9fa98
mkmer <m_kasper@sbcglobal.net>
2023-06-22 18:14:07 +0000

    typo in wrapper

--- blinkpy/helpers/util.py
+++ blinkpy/helpers/util.py
@@ -163,7 +163,7 @@ class Throttle:
             now = int(time.time())
             last_call_delta = now - self.last_call
             if force or last_call_delta > self.throttle_time:
-                result = method(*args, *kwargs)
+                result = method(*args, **kwargs)
                 self.last_call = now
                 return result
 
