a9c7c407790807dd086a32fa5353ca53ecdbfb05
Kevin Fronczak <kfronczak@gmail.com>
2023-11-19 12:16:26 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,7 @@
 #
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('../blinkpy'))
-os.system("python -m build ..")
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
